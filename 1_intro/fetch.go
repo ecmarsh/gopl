989c9bc60e9c725d9041c04d8d17b90046a47f6a
Ethan Marsh <emarsh55@gmail.com>
2019-09-28 14:42:26 -0700

    1.7: Replace ReadAll with io.Copy

--- 1_intro/fetch.go
+++ 1_intro/fetch.go
@@ -2,26 +2,23 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-    for _, url := range os.Args[1:] {
-      res, err := http.Get(url)
-      if err != nil {
-        fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-        os.Exit(1)
-      }
-      body, err := ioutil.ReadAll(res.Body)
-      res.Body.Close()
-      if err != nil {
-        fmt.Fprintf(os.Stderr, "fetch: %s: %v\n", url, err)
-        os.Exit(1)
-      }
-      fmt.Printf("%s", body)
-    }
+	for _, url := range os.Args[1:] {
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		if _, err := io.Copy(os.Stdout, res.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		res.Body.Close()
+	}
 }
-

cf6a4b16b5c86fee5e5df538fc27529119d6f701
Ethan Marsh <emarsh55@gmail.com>
2019-10-15 20:27:07 -0700

    Function values - html doc tree traversal

--- 5_functions/funcvals/funcvals.go
+++ 5_functions/funcvals/funcvals.go
@@ -1,9 +1,15 @@
 // Package funcvals includes examples for function values.
 package funcvals
 
-import "fmt"
+import (
+	"fmt"
 
-// Node represents an html Node for this example
+	"golang.org/x/net/html"
+)
+
+/*
+From package html for reference:
+
 type Node struct {
 	Type                    NodeType
 	Data                    string
@@ -11,10 +17,8 @@ type Node struct {
 	FirstChild, NextSibling *Node
 }
 
-// NodeType of html Node
 type NodeType int32
 
-// Specific html Node types
 const (
 	ErrorNode NodeType = iota
 	TextNode
@@ -24,21 +28,21 @@ const (
 	DoctypeNode
 )
 
-// Attribute for html Node
 type Attribute struct {
 	Key, Val string
 }
+*/
 
-// forEachNode calls the functions pre(x) and post(x) for each node
+// ForEachNode calls the functions pre(x) and post(x) for each node
 // x in the tree rooted at n. Both functions are optional.
 // pre is called before the children are visited (preorder) and
 // post is called after (postorder).
-func forEachNode(n *Node, pre, post func(n *Node)) {
+func ForEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		forEachNode(c, pre, post)
+		ForEachNode(c, pre, post)
 	}
 	if post != nil {
 		post(n)
@@ -47,14 +51,14 @@ func forEachNode(n *Node, pre, post func(n *Node)) {
 
 var depth int
 
-func startElement(n *Node) {
-	if n.Type == ElementNode {
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
 		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
 		depth++
 	}
 }
-func endElement(n *Node) {
-	if n.Type == ElementNode {
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
 		depth--
 		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
@@ -64,4 +68,4 @@ func endElement(n *Node) {
 // variable number of spaces where the width and string
 // are provided by arguments depth*2 and "".
 
-// Usage: forEachNode(doc, startElement, endElement)
+// Usage: ForEachNode(doc, startElement, endElement)

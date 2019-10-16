48340c6d96f6c9901515569c3071b313a2a59bb8
Ethan Marsh <emarsh55@gmail.com>
2019-10-15 20:27:33 -0700

    Fetch and print title - no deferring version

---
+++ 5_functions/defer/title.go
@@ -0,0 +1,52 @@
+// Package main fetches an HTML document and prints title
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"../funcvals"
+	"golang.org/x/net/html"
+)
+
+// Attribute for html Node
+type Attribute struct {
+	Key, Val string
+}
+
+// Print HTML doc title or error if not Content-Type HTML
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	// Check Content-Type is HTML (text/html; charset=utf-8)
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	funcvals.ForEachNode(doc, visitNode, nil)
+	return nil
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		if err := title(arg); err != nil {
+			fmt.Fprintf(os.Stderr, "title: %v\n", err)
+		}
+	}
+}

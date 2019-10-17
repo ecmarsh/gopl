9891058aa4cd4905e2ba37579b2acbba350b670b
Ethan Marsh <emarsh55@gmail.com>
2019-10-16 17:46:11 -0700

    Change multiple close calls to one deferred call

--- 5_functions/defer/title.go
+++ 5_functions/defer/title.go
@@ -17,7 +17,7 @@ type Attribute struct {
 }
 
 // Print HTML doc title or error if not Content-Type HTML
-func title(url string) error {
+func titlenodefer(url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -43,9 +43,35 @@ func title(url string) error {
 	return nil
 }
 
+// Print HTML doc title or error if not Content-Type HTML
+func titledefer(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close() // Executes after function completion
+
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	funcvals.ForEachNode(doc, visitNode, nil)
+	return nil
+}
+
 func main() {
 	for _, arg := range os.Args[1:] {
-		if err := title(arg); err != nil {
+		if err := titledefer(arg); err != nil {
 			fmt.Fprintf(os.Stderr, "title: %v\n", err)
 		}
 	}
